7fc7a1438341fb8a8d19103a04f19562fa465fe0
gopikrishnayarlagadda <yarlagaddagopi84@gmail.com>
2021-07-18 21:31:15 +0530

    frist commit

---
+++ ntier-tf-asw1/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-2"
+}
+# we need to create a vpc resource
+resource "aws_vpc" "ntiervpc" {
+    cidr_block = "192.168.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+      "Name" = "from tf"
+    }
+  
+}
